a8e18ecca8143df4cb821d3fc4de55c95acdf198
rizqims <rizqims100@gmail.com>
2022-06-25 08:14:48 +0700

    adding Vue

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            width: 'w-100',
-            burger: 24,
-            owl : 'assets/img/a.jpg' 
+            items: [
+                { kelas: 'Bronze', primaryC: '#804A00', secondaryC: '#B08D57', harga: 100000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
+                { kelas: 'Silver', primaryC: '#616161', secondaryC: '#E6E7E8', harga: 100000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
+                { kelas: 'Gold', primaryC: '#AE8625', secondaryC: '#F7EF8A', harga: 100000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
+            ]
+        }
+    },
+    methods:{
+        gradients(colorOne, colorTwo) {
+            return `background-image: linear-gradient(135deg, ${colorOne} 0%, ${colorTwo} 100%);`
         }
     }
 })

abcfdc34b178b6f8eac6b1880549c66d80de2133
rizqims <rizqims100@gmail.com>
2022-06-27 20:42:30 +0700

    re-init Bootstrap

--- Bootstrap/mainBootstrap.js
+++ Bootstrap/mainBootstrap.js
@@ -3,8 +3,8 @@ const app = Vue.createApp({
         return {
             items: [
                 { kelas: 'Bronze', primaryC: '#804A00', secondaryC: '#B08D57', harga: 100000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
-                { kelas: 'Silver', primaryC: '#616161', secondaryC: '#E6E7E8', harga: 100000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
-                { kelas: 'Gold', primaryC: '#AE8625', secondaryC: '#F7EF8A', harga: 100000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
+                { kelas: 'Silver', primaryC: '#616161', secondaryC: '#E6E7E8', harga: 200000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
+                { kelas: 'Gold', primaryC: '#AE8625', secondaryC: '#F7EF8A', harga: 500000, benefits: ['lorem ipsum dolor sit amet', 'Dignissimos eaque ipsam quas deserunt', 'Iure commodi fuga quis praesentium?'] },
             ]
         }
     },
